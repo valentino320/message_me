6e9bfd1b6af0f484d07c0a852fbdbeacc127190c
valentino320 <valentino320@gmail.com>
2021-01-12 11:14:35 +0000

    root and login route set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+root 'chatroom#index'
+get 'home', to: 'pages#home'
+get "about", to: "pages#about"
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
