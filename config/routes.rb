225c468b34e1806ee02415ab9407d4b4a1a87b73
valentino320 <valentino320@gmail.com>
2021-01-14 11:22:23 +0000

    websocket not working

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ get "about", to: "pages#about"
 get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
+post 'message', to: 'messages#create'
+
+mount ActionCable.server  => '/cable'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
